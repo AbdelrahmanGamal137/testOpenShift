8936b405abf4e95fa647e77a0931fcc13d930f43
TRADENET\ayousef <abdelrahman.gamal137@gmail.com>
2022-05-29 12:50:40 +0200

    test config

--- app-test-main/src/main/java/com/example/demo/restcontroller/TestRestController.java
+++ app-test-main/src/main/java/com/example/demo/restcontroller/TestRestController.java
@@ -1,5 +1,6 @@
 package com.example.demo.restcontroller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/app")
 public class TestRestController {
 
+    @Value("${test.property.1:t}")
+    String test;
+
     @GetMapping("/get")
     public String getMethod(){
-        return "app rest connected new build v 3";
+        return "app rest connected new build v 3 : " + test;
     }
 
 }
